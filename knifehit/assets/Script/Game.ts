f687b0f82764e77da26c4cb8eea4eeb7b5a7b3b9
gawao <gawao57@gmail.com>
2018-07-06 19:38:09 +0900

    Initial commit

---
+++ knifehit/assets/Script/Game.ts
@@ -0,0 +1,33 @@
+// Learn TypeScript:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'gamedayo';
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        cc.director.setClearColor(cc.color(68, 68, 68));
+    }
+
+    start () {
+
+    }
+
+    // update (dt) {}
+}
